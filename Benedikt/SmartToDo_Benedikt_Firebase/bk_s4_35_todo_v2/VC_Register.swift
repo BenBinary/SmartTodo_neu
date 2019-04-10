0482a8e6ee897ad4c9af81cc7b1bbc6981a0b4c7
Bene <bk191414@gmail.com>
2019-04-10 21:57:15 +0200

    Upload '

---
+++ Benedikt/SmartToDo_Benedikt_Firebase/bk_s4_35_todo_v2/VC_Register.swift
@@ -0,0 +1,30 @@
+//
+//  VC_Register.swift
+//  bk_s4_35_todo_v2
+//
+//  Created by Benedikt Kurz on 09.04.19.
+//  Copyright © 2019 Benedikt Kurz. All rights reserved.
+//
+
+import UIKit
+
+class VC_Register: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
